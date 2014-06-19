c4030284662c7dce5fc57423df6c35b201bdeaae
arnau126 <arnauga3@hotmail.com>
2014-06-19 13:10:30 +0200

    Update MessageFrame.java

--- PracAS/src/com/pracas/presentation/MessageFrame.java
+++ PracAS/src/com/pracas/presentation/MessageFrame.java
@@ -60,7 +60,7 @@ public class MessageFrame extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        this.setVisible(false);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
