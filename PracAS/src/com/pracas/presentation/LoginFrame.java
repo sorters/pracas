e6bc02091c4d271a08fc15b5e010287ce6e6727a
arnau126 <arnauga3@hotmail.com>
2014-06-19 13:53:29 +0200

    Update LoginFrame.java

--- PracAS/src/com/pracas/presentation/LoginFrame.java
+++ PracAS/src/com/pracas/presentation/LoginFrame.java
@@ -125,7 +125,7 @@ public class LoginFrame extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {
-		
+		cp.showMessage("hey hou");
 	}
 
 	private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
