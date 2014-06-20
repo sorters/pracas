2cd055e48bdd5334042f42581891e28651df1fc1
arnau126 <arnauga3@hotmail.com>
2014-06-20 23:38:23 +0200

    Update CtrlPresentation.java

--- PracAS/src/com/pracas/presentation/CtrlPresentation.java
+++ PracAS/src/com/pracas/presentation/CtrlPresentation.java
@@ -5,6 +5,7 @@
 package com.pracas.presentation;
 
 import com.pracas.domain.controller.CtrlJugarPartida;
+import com.pracas.exception.InvalidLetterException;
 import com.pracas.exception.UserIsNotPlayerException;
 import com.pracas.exception.UsernameNotExistsException;
 import com.pracas.exception.WrongPasswordException;
@@ -56,13 +57,29 @@ public class CtrlPresentation {
         }
     }
     
-    public void OKPressedPartida(int pos, String lletra) {
-    	// TO DO
-    	partidaFrame.afegirLletra(pos, lletra);
+    public void OKPressedPartida(int pos, char lletra) {
+    	/*try {
+			ResponseType info = cjp.ferJugada(pos, lletra);
+			if (info.acabada && !info.guanyada) {
+				String msg = "Llastima! No has endevinat la paraula... Mes sort la propera vegada."; 
+				showMessage(msg);
+			}
+			else if (info.acabada) {
+				String msg = "Felicitats has endevinat la paraula. T'hem enviat un correu. :)"; 
+				showMessage(msg);
+			}
+			else {
+				partidaFrame.setPuntuacio(info.puntuacio);
+				partidaFrame.setNumError(info.errors);
+				if (info.encert) partidaFrame.afegirLletra(pos, lletra);	
+			}
+		} catch (InvalidLetterException e) {
+			showMessage("La lletra introduida no es un caracter valid!");
+		}*/
     }
     
     public void CancelPressedPartida() {
-    	// TO DO
+    	// TODO
     }
     
 }
